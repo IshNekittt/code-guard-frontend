6cbc0068932473ad544bce1cf7a6a81620d99c23
Olga Ferubko <ferubko.olga@gmail.com>
2025-04-18 19:43:15 +0300

    login without styles

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,14 @@
+import LoginForm from "../../components/LoginForm/LoginForm";
+import { Helmet } from "react-helmet-async";
+import s from "./LoginPage.module.css";
+
+export default function LoginPage() {
+    return (
+        <div className={s.loginPage}>
+            <Helmet>
+                <title>Login</title>
+            </Helmet>
+            <LoginForm />
+        </div>
+    );
+}
