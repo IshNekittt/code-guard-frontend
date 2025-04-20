ca60c09a7567e23ac8921c8f7b5aeaca3357a129
elina <elinaslvv@gmail.com>
2025-04-20 18:11:28 +0300

    First commit header and logout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,16 @@ import { getTransactions } from "../redux/transactionsOp";
 import TransactionsList from "./transactions/TransactionsList";
 import { useDispatch } from "react-redux";
 import ButtonAddTransactions from "./transactions/ButtonAddTransactions";
+import HeaderWithLogoutModal from "./HeaderWithLogoutModal/HeaderWithLogoutModal";
 
-function App() {
+export default function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getTransactions());
   }, [dispatch]);
   return (
     <>
+      <HeaderWithLogoutModal />
       <TransactionsList />
       <ButtonAddTransactions />
     </>
