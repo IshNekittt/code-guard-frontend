db7c8ab63f5a79fca030b26e71f831beadc538e8
Viktor_Baratejj <Viktor_baratejj@ukr.net>
2025-04-21 01:15:45 +0300

    add fix sidebar

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,7 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import "./index.css";
 import App from "./components/App";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
