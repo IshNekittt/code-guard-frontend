d41c7947391ed6117b52d14e0d11063e98e1de61
Olga Ferubko <ferubko.olga@gmail.com>
2025-04-22 21:10:59 +0300

    styles for login

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,13 +1,9 @@
 import LoginForm from "../../components/LoginForm/LoginForm";
-import { Helmet } from "react-helmet-async";
-import s from "./LoginPage.module.css";
 
 export default function LoginPage() {
     return (
         <div className="s.loginPage">
-            <Helmet>
-                <title>Login</title>
-            </Helmet>
+            <title>Login</title>
             <LoginForm />
         </div>
     );
