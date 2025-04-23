a6f7086c03886ae7402006bbf218373144cb2137
Nekittt <nikitaishchenko334@gmail.com>
2025-04-23 10:13:57 +0300

    Add error page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import LoginPage from "../pages/LoginPage/LoginPage";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="login" element={<LoginPage />} />
+
+        <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
   );
