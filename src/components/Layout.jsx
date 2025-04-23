075686e8fdaff910c36605f40ff9659a5ffdb494
Nekittt <nikitaishchenko334@gmail.com>
2025-04-23 09:55:08 +0300

    Fix occurred errors

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
-import AppBar from "../components/AppBar/AppBar";
 
 export default function Layout() {
-    return (
-        <div>
-            <AppBar />
-            <Outlet />
-        </div>
-    );
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
