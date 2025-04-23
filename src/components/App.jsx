de3ed91ce24a5cd2ba36b80e8913207a998c047b
Nekittt <nikitaishchenko334@gmail.com>
2025-04-23 16:47:59 +0300

    Refactor the router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,25 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import HomePage from "../pages/HomePage/HomePage";
+
+import RestrictedRoute from "./RestrictedRoute";
+import { Suspense } from "react";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="login" element={<LoginPage />} />
+    <Suspense fallback={null}>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/login"
+            element={<RestrictedRoute route={<LoginPage />} />}
+          />
 
-        <Route path="*" element={<ErrorPage />} />
-      </Route>
-    </Routes>
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Layout>
+    </Suspense>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,3 @@
-import { useEffect } from "react";
-import { fetchContacts } from "../../redux/contacts/operations";
-import { useDispatch } from "react-redux";
-
-
-
 export default function HomePage() {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchContacts());
-    }, [dispatch]);
-
-    return (
-        <div>
-                <title>Home</title>
-        </div>
-    );
+  return <div></div>;
 }
