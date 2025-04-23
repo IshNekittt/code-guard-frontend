e0ffd28e3dee712987626a32e4dacc79898eb70d
Nekittt <nikitaishchenko334@gmail.com>
2025-04-23 10:09:32 +0300

    Add token persisting

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,17 @@ import App from "./components/App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
