b96b14f9f4c481287295cdeeceb8fc4bd577b5b3
Olga Ferubko <ferubko.olga@gmail.com>
2025-04-23 18:54:51 +0300

    correct routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,20 @@ import Layout from "./Layout";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import HomePage from "../pages/HomePage/HomePage";
+import { Suspense } from "react";
+import RestrictedRoute from "./RestrictedRoute";
 
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="home" element={<HomePage />} />
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
+  <Suspense fallback={null}>
+    <Layout>
+      <Routes>
+        <Route path="/login" element={<RestrictedRoute route={<LoginPage />} />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Layout>
+  </Suspense>
   );
 }
