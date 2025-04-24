cede64209914c2c8dd2f4cc6857544b49478dfc9
Viktor_Baratejj <Viktor_baratejj@ukr.net>
2025-04-24 18:59:18 +0300

    add fix sectoin + statistics

---
+++ src/components/Sidebar/StatisticsTest.jsx
@@ -0,0 +1,18 @@
+const Statistics = () => {
+    return (
+      <div
+        style={{
+          color: 'white',
+          padding: '20px',
+          fontWeight: '700',
+          fontSize: '24px',
+          textAlign: 'center',
+        }}
+      >
+        "Statistics Test"
+      </div>
+    );
+  };
+  
+  export default Statistics;
+  
