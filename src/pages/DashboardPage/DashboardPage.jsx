0c0b81ef57eb0d268de704a30e23615e53d4e4a6
Andrii-Petrunia <petrunyandrey@gmail.com>
2025-04-27 17:00:26 +0200

    "feat: интегрировать кнопку и модалку добавления транзакции в DashboardPage"

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,24 +1,64 @@
-import { Outlet, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useLocation, Outlet } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getTransactions, addTransaction } from "../../redux/transactionsOp";
+import { CATEGORIES } from "../../constants/categories";
 import SideBar from "../../components/Sidebar/Sidebar";
 import TransactionsList from "../../components/transactions/TransactionsList";
+import ButtonAddTransactions from "../../components/transactions/ButtonAddTransactions";
+import ModalAddTransaction from "../../components/transactions/ModalAddTransaction";
 import css from "./DashboardPage.module.css";
 
 const DashboardPage = () => {
+  const dispatch = useDispatch();
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    dispatch(getTransactions());
+    setCategories(CATEGORIES);
+  }, [dispatch]);
+
+  const handleCreate = async (data) => {
+    try {
+      dispatch(addTransaction({ ...data, date: data.date.toISOString() }));
+      setModalOpen(false);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const { pathname } = useLocation();
   const isBalancePage = pathname === "/home";
   const isExchangeRatesPage = pathname === "/home/exchange-rates";
   const isStatisticsPage = pathname === "/home/statistics";
+
   return (
-    <div className={css.loyat}>
+    <div className={css.layout}>
       <div className={css.sideBar}>
         <SideBar />
       </div>
+
       <div className={css.transactionsContainer}>
+        {/* Вложенные маршруты (Balance, ExchangeRates, Statistics) */}
         {!(isBalancePage || isExchangeRatesPage || isStatisticsPage) && (
           <Outlet />
         )}
-        {/* {!isBalancePage && <Outlet />} */}
-        {/* <Outlet /> */}
+
+        {/* Кнопка и модалка добавления транзакции */}
+        <ButtonAddTransactions
+          onClick={() => {
+            console.log("Кнопка нажата"); setModalOpen(true);
+          }}
+        />
+        <ModalAddTransaction
+          isOpen={isModalOpen}
+          onClose={() => setModalOpen(false)}
+          categories={categories}
+          onCreate={handleCreate}
+        />
+
+        {/* Список транзакций */}
         <TransactionsList />
       </div>
     </div>
