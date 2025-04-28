0ee4feb5a5ea654a3326a9c4bbecc5434becc7d5
Nekittt <nikitaishchenko334@gmail.com>
2025-04-28 16:11:14 +0300

    Add normalize

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import "./index.css";
+import "modern-normalize";
 import App from "./components/App";
 import { Toaster } from "react-hot-toast";
 import { PersistGate } from "redux-persist/lib/integration/react";
