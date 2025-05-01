cbad28221b49b392cc13f1c97f43b88e62b76fdb
AntonK33 <antonkulackov@gmail.com>
2025-05-01 12:09:42 +0200

    selector

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -2,6 +2,6 @@ export const selectUser = (state) => state.auth.user;
 export const selectToken = (state) => state.auth.token;
 export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 export const selectIsRefreshing = (state) => state.auth.isRefreshing;
-//export const selectStatistics = (state) => state.statistics.transactions || [];
+
 export const selectStatistics = (state) => state.statistics.transactions.data || [];
 export const selectBalanсe = (state) => state.finance.totalBalance;
